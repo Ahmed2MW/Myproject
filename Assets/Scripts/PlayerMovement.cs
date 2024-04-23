0cccc2522f5f68fe132bc76e0f56228878144d47
Ahmed2MW <163757638+Ahmed2MW@users.noreply.github.com>
2024-04-23 02:16:31 +0200

    Ajout personnage et mouvemant
    Pour le mouvement targetVelocity me cause de probleme, le player m'affiche l'erreur et je n'arrive pas compilé le jeux en game
    la contunie le reste apres ...

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector3 velocity = Vector3.zero;
+
+ void FixedUpdate()
+ {
+    float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+    MovePlayer(horizontalMovement);
+
+    }
+
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 velocity = new Vector2(_horizontalMovement, rb.velocity.y);
+      
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity , ref velocity, .05f);
+    }
+}
+
